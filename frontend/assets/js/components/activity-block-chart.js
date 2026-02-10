c9ed9440945045b9a81779a57974ceb266fa52df
User <user@example.com>
2026-02-09 22:06:22 -0500

    style: move activity chart label to end of blocks

--- frontend/assets/js/components/activity-block-chart.js
+++ frontend/assets/js/components/activity-block-chart.js
@@ -74,7 +74,7 @@
                 return `<div class="${classes.join(' ')}"></div>`;
             }).join('');
 
-            this.container.innerHTML = `<div class="activity-block-grid"><span class="activity-block-label">Last ${this.daysToShow}:</span>${blocks}</div>`;
+            this.container.innerHTML = `<div class="activity-block-grid">${blocks}<span class="activity-block-label">Last ${this.daysToShow}</span></div>`;
         }
 
         formatDateKey(date) {
